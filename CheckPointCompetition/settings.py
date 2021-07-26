d58e5ea37d476144bdc94c4b21c97a0d5c005e11
Pijukatel <Pijukatel@users.noreply.github.com>
2021-07-26 15:08:58 +0100

    Proper app subfolder and homepage placeholder.

--- CheckPointCompetition/settings.py
+++ CheckPointCompetition/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'CheckPointCompetition.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
