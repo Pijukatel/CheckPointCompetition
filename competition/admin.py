cf25ce6aaab8e38f381516d86e059730e8b3f10d
Pijukatel <Pijukatel@users.noreply.github.com>
2021-08-02 19:18:02 +0100

    Adding new models to add allow teams.

--- competition/admin.py
+++ competition/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import CheckPoint
-
+from .models import CheckPoint, Membership, Point, Team
 
 @admin.register(CheckPoint)
 class AdminCheckpoint(admin.ModelAdmin):
-    """Admin view for Package."""
+    pass
+
+@admin.register(Membership)
+class AdminMembership(admin.ModelAdmin):
+    pass
+
+@admin.register(Point)
+class AdminPoint(admin.ModelAdmin):
+    pass
+
+@admin.register(Team)
+class AdminTeam(admin.ModelAdmin):
+    pass
--- competition/models.py
+++ competition/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -9,3 +11,36 @@ class CheckPoint(models.Model):
     gps_lat = models.FloatField()
     gps_lon = models.FloatField()
     photo = models.ImageField(upload_to='checkpoints')
+
+
+class Point(models.Model):
+    photo = models.ImageField(upload_to='points')
+    checkpoint = models.ForeignKey(
+        CheckPoint, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    confirmed = models.BooleanField(default=False)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+    photo = models.ImageField(upload_to='teams')
+    confirmed = models.BooleanField(default=False)
+
+
+class Membership(models.Model):
+    """Intermediate model for user being able to be member of a group."""
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    team = models.ForeignKey(Team,on_delete=models.CASCADE, null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """Using signals to create membership when user is created."""
+    if created:
+        Membership.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """Using signals to save membership when user is saved."""
+    instance.membership.save()
