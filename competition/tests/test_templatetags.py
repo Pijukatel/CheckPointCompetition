0c95d859d864803f6ac1a4715ef33cab0d681c79
Pijukatel <Pijukatel@users.noreply.github.com>
2021-08-18 19:01:16 +0100

    Add templatetag test.

---
+++ competition/tests/test_templatetags.py
@@ -0,0 +1,14 @@
+import pytest
+from django.contrib.auth.models import User
+
+from .globals_for_tests import G
+from ..models import Team
+from ..templatetags.team_of_user import team_of_user
+
+
+@pytest.mark.usefixtures('load_registered_user1_with_team1')
+@pytest.mark.django_db
+def test_team_of_user():
+    user = User.objects.get(username=G.user1_name)
+    team = Team.objects.get(name=G.team1_name)
+    assert team.name == team_of_user(user)
