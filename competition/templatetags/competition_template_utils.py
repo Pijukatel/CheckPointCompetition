8672ea455b964836c121f97c39b35ed829be2d83
Pijukatel <Pijukatel@users.noreply.github.com>
2021-08-31 22:40:00 +0100

    Added map to checkpoint list. Map can show multiple points.

--- competition/templatetags/competition_template_utils.py
+++ competition/templatetags/competition_template_utils.py
@@ -1,12 +1,17 @@
-
-from ..models import Team, Membership
+from ..models import Membership
 
 from django import template
 
 register = template.Library()
+
 
 @register.simple_tag
 def team_of_user(user):
     team = Membership.objects.get(user=user).team
     if team:
         return f"{team.name}"
+
+
+@register.simple_tag
+def to_list(*args):
+    return args
--- competition/tests/test_templatetags.py
+++ competition/tests/test_templatetags.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 from .globals_for_tests import G
 from ..models import Team
-from ..templatetags.team_of_user import team_of_user
+from ..templatetags.competition_template_utils import team_of_user
 
 
 @pytest.mark.usefixtures('load_registered_user1_with_team1')
