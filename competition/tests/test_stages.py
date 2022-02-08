599af6edbde4ec65c6a1dea7e28afb73b18edd8a
Pijukatel <Pijukatel@users.noreply.github.com>
2022-02-08 22:21:05 +0100

    Open for registration + tests.

--- competition/tests/test_stages.py
+++ competition/tests/test_stages.py
@@ -33,10 +33,95 @@ from .globals_for_tests import G
         ("home.html", "/team/photo-confirm/"),
         ("home.html", "/teams/"),
     ])
-def test_template_by_url_with_anonymous_require_login(client, template, url, far_future):
+def test_template_in_countdown(client, template, url, far_future):
     """Test that all endpoints are redirected to home in countdown stage."""
     response = client.get(url, follow=True)
     assert response.status_code == 200
     assertTemplateUsed(response, "/".join([G.APP_NAME, template]))
     assert f"Competition will be open for registration: {far_future.isoformat()}" in html.unescape(
         response.content.decode(response.charset))
+
+
+@pytest.mark.disable_default_time
+@pytest.mark.usefixtures("in_pre_registration")
+@pytest.mark.usefixtures("load_registered_user1_with_team1")
+@pytest.mark.usefixtures("load_checkpoint1")
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "template, url", [
+        ("home.html", ""),
+        ("home.html", "/checkpoints/"),
+        ("home.html", f"/checkpoints/{G.checkpoint1_name}"),
+        ("home.html", "/point/photo-confirm/"),
+    ])
+def test_template_in_pre_registration_closed_views(client_with_logged_user1, template, url, far_future):
+    """Test that all endpoints are redirected to home in countdown stage."""
+    response = client_with_logged_user1.get(url, follow=True)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "/".join([G.APP_NAME, template]))
+    assert f"Competition will start: {far_future.isoformat()}" in html.unescape(
+        response.content.decode(response.charset))
+
+
+@pytest.mark.disable_default_time
+@pytest.mark.usefixtures("in_pre_registration")
+@pytest.mark.usefixtures("load_registered_user1_with_team1")
+@pytest.mark.usefixtures("load_checkpoint1")
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "template, url", [
+        ("register.html", "/accounts/register/"),
+        ("login.html", "/accounts/login/"),
+        ("login.html", "/accounts/logout/"),
+        ("user_detail.html", "/accounts/user/"),
+        ("user_list.html", "/accounts/users/"),
+        ("user_delete.html", "/accounts/user/delete/"),
+        ("user_update.html", "/accounts/user/update/"),
+        ("user_detail.html", "/team/leave/"),
+        ("team_detail.html", f"/team/{G.team1_name}/"),
+        ("team_update.html", f"/team/{G.team1_name}/update/"),
+        ("team_delete.html", f"/team/{G.team1_name}/delete/"),
+        ("team_add_member.html", f"/team/{G.team1_name}/add_member/"),
+        ("login.html", "/team/photo-confirm/"),  # Redirect to staff
+        ("team_list.html", "/teams/"),
+    ])
+def test_template_in_pre_registration_open_views(client_with_logged_user1, template, url, far_future):
+    """Test that all endpoints are redirected to home in countdown stage."""
+    response = client_with_logged_user1.get(url, follow=True)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "/".join([G.APP_NAME, template]))
+
+
+@pytest.mark.disable_default_time
+@pytest.mark.usefixtures("in_pre_registration")
+@pytest.mark.usefixtures("load_registered_user1_with_team1")
+@pytest.mark.usefixtures("load_checkpoint1")
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "template, url", [
+        ("team_create.html", "/team/create/"),  # Is actually without team.
+        ("team_photo_confirmation.html", "/team/photo-confirm/"),
+        ("photo_confirmation_empty.html", "/point/photo-confirm/"),
+    ])
+def test_template_in_pre_registration_open_views_by_staff(client_with_logged_user_staff, template, url, far_future):
+    """Test that all endpoints are redirected to home in countdown stage."""
+    response = client_with_logged_user_staff.get(url, follow=True)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "/".join([G.APP_NAME, template]))
+
+
+@pytest.mark.disable_default_time
+@pytest.mark.usefixtures("in_pre_registration")
+@pytest.mark.usefixtures("load_registered_user1_with_confirmed_team1")
+@pytest.mark.usefixtures("load_checkpoint1")
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "template, url", [
+        ("home.html", "/point/photo-confirm/"),
+    ])
+def test_template_in_pre_registration_open_views_by_staff_point_confirm(client_with_logged_user_staff, template, url,
+                                                                        far_future):
+    """Test that all endpoints are redirected to home in countdown stage."""
+    response = client_with_logged_user_staff.get(url, follow=True)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "/".join([G.APP_NAME, template]))
