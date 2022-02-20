edf6e8af42eca59ff545b91d531d3d62f307a02f
Pijukatel <pepek.pepkovatej@seznam.cz>
2022-02-20 23:07:25 +0100

    Update to latest OpenLayers. (#71)
    Periodic Update of user positons.
    
    Co-authored-by: Pijukatel <Pijukatel@users.noreply.github.com>

--- competition/static/js/map.js
+++ competition/static/js/map.js
@@ -1,78 +1,90 @@
-const OpenLayers = window.OpenLayers
-const pointStyle =
-{
-    cursor : "inherit",
-    fillColor : "#666666",
-    fillOpacity : 0.9,
-    fontColor : "#000000",
-    hoverFillColor : "white",
-    hoverFillOpacity : 0.8,
-    hoverPointRadius : 1,
-    hoverPointUnit : "%",
-    hoverStrokeColor : "red",
-    hoverStrokeOpacity : 1,
-    hoverStrokeWidth : 0.2,
-    labelAlign : "cm",
-    labelOutlineColor : "white",
-    labelOutlineWidth : 3,
-    pointRadius : 6,
-    pointerEvents : "visiblePainted",
-    strokeColor : "#ee9900",
-    strokeDashstyle : "solid",
-    strokeLinecap : "round",
-    strokeOpacity : 0.3,
-    strokeWidth : 1
-}
+const ol = window.ol
+
+
+const styles = {
+  'Checkpoint': new ol.style.Style({
+    image: new ol.style.Circle({
+      radius: 5,
+      fill: new ol.style.Fill({color: '#d71616'}),
+      stroke: new ol.style.Stroke({color: '#000000', width: 1}),
+    }),
+  }),
+  'User': new ol.style.Style({
+    image: new ol.style.Circle({
+      radius: 5,
+      fill: new ol.style.Fill({color: '#d7d416'}),
+      stroke: new ol.style.Stroke({color: '#000000', width: 1}),
+    }),
+  })
+
+};
 
 function createBaseLayerMap() {
-    const map = new OpenLayers.Map("map");
-    map.addLayer(new OpenLayers.Layer.OSM());
+    const map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([11.974560, 57.68]),
+            zoom: 12
+        })
+    });
+    const userSource = new ol.source.Vector({});
+    const userLayer = new ol.layer.Vector({source: userSource,});
 
-    const epsg4326 =  new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
-    const projectTo = map.getProjectionObject(); //The map projection (Spherical Mercator)
+    const checkpointSource = new ol.source.Vector({});
+    const checkpointLayer = new ol.layer.Vector({source: checkpointSource,});
 
-    let lonLat = new OpenLayers.LonLat( 11.974560 , 57.708870 ).transform(epsg4326, projectTo);
+    map.addLayer(userLayer);
+    map.addLayer(checkpointLayer);
 
-    let zoom=12;
-    map.setCenter (lonLat, zoom);
-    // map.viewPortDiv.style["overflow"]="visible";
-    map.viewPortDiv.style["position"]="absolute";
-    map.viewPortDiv.style["z-index"]="900";
 
-    // Define markers as "features" of the vector layer:
-    const vectorLayer = new OpenLayers.Layer.Vector("Overlay");
-    map.addLayer(vectorLayer);
-    function featureFactory(lon, lat, style) {
-        return new OpenLayers.Feature.Vector(
-            new OpenLayers.Geometry.Point( lon, lat).transform(epsg4326, projectTo),
-            {description:"This is the value of<br>57, 16"},
-            (style));
-    }
-    return {"vectorLayer": vectorLayer, "featureFactory":featureFactory}
-
+    return {"checkpointSource": checkpointSource, "userSource": userSource}
 }
 
 
 function fullMap() {
-    const map = createBaseLayerMap()
+    const sources = createBaseLayerMap()
     // Add all checkpoints to map once.
-    addCheckpoints(map)
+    addCheckpoints(sources.checkpointSource)
 
     // Periodically update user positions in map.
     //TODO add users do different layer
+    setInterval(()=>{
+        sources.userSource.clear()
+        addUsers(sources.userSource)
+    }, 5000)
 
-    //map.vectorLayer.addFeatures(map.featureFactory(12, 57.708870, pointStyle))
-    //map.vectorLayer.addFeatures(map.featureFactory(12.02, 57.708870, pointStyle))
+
+
+
 
 }
 
-function addCheckpoints(map) {
-    requestCheckpoints(function(checkpoints) {
-        JSON.parse(checkpoints).forEach(addCheckpointToMap);
+function addMarker(mapSource, markerStyle, requestCallback){
+     requestCallback(function(point) {
+        JSON.parse(point).forEach(addPointToMap);
     })
-    function addCheckpointToMap(checkpoint) {
-        map.vectorLayer.addFeatures(map.featureFactory(checkpoint.gps_lon, checkpoint.gps_lat, pointStyle))
+
+    function addPointToMap(point) {
+        const iconFeature = new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.fromLonLat([point.gps_lon, point.gps_lat])),
+            name: 'Whatever'
+        });
+        iconFeature.setStyle(markerStyle);
+        mapSource.addFeature(iconFeature);
     }
+}
+
+function addCheckpoints(mapSource) {
+    addMarker(mapSource, styles['Checkpoint'], requestCheckpoints)
+}
+
+function addUsers(mapSource) {
+    addMarker(mapSource, styles['User'], requestUsers)
 }
 
 
