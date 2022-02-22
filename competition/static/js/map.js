eb72bf2123b3bbe0c67f7286b38368e241035450
Pijukatel <Pijukatel@users.noreply.github.com>
2022-02-22 23:19:33 +0100

    Changing GET api to fetch from XMLHttpRequest. Coloring users by team. Different visu for team and checkpoint.

--- competition/static/js/map.js
+++ competition/static/js/map.js
@@ -2,21 +2,30 @@ const ol = window.ol
 
 const element = document.getElementById('popup');
 
+const userColors = Array.from({ length: 100 }, () => {
+  return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+});
+
+let teamStyleMapping = {}
+
 const styles = {
   'Checkpoint': new ol.style.Style({
-    image: new ol.style.Circle({
-      radius: 5,
+    image: new ol.style.RegularShape({
+      points: 3,
+      radius: 10,
+      angle: 0,
       fill: new ol.style.Fill({color: '#d71616'}),
       stroke: new ol.style.Stroke({color: '#000000', width: 1}),
     }),
   }),
-  'User': new ol.style.Style({
-    image: new ol.style.Circle({
+  'Users': userColors.map( (color)=> {return new ol.style.Style({
+      image: new ol.style.Circle({
       radius: 5,
-      fill: new ol.style.Fill({color: '#d7d416'}),
+      fill: new ol.style.Fill({color: color}),
       stroke: new ol.style.Stroke({color: '#000000', width: 1}),
     }),
-  })
+  })})
+
 
 };
 
@@ -55,15 +64,16 @@ function createBaseLayerMap() {
         return feature;
       });
       if (feature) {
-        popup.setPosition(evt.coordinate);
-        $(element).popover({
-          placement: 'top',
-          html: true,
-          content: feature.get('name'),
-        });
-        $(element).popover('show');
+          $(element).popover('dispose');
+          popup.setPosition(evt.coordinate);
+          $(element).popover({
+              placement: 'top',
+              html: false,
+              content: feature.get('name'),
+          });
+          $(element).popover('show');
       } else {
-        $(element).popover('dispose');
+          $(element).popover('dispose');
       }
     });
 
@@ -78,8 +88,6 @@ function createBaseLayerMap() {
       $(element).popover('dispose');
     });
 
-
-
     return {"checkpointSource": checkpointSource, "userSource": userSource}
 }
 
@@ -90,39 +98,46 @@ function fullMap() {
     addCheckpoints(sources.checkpointSource)
 
     // Periodically update user positions in map.
-    //TODO add users do different layer
     setInterval(()=>{
         sources.userSource.clear()
         addUsers(sources.userSource)
     }, 5000)
 
 
-
-
-
 }
 
-function addMarker(mapSource, markerStyle, requestCallback){
-     requestCallback(function(point) {
-        JSON.parse(point).forEach(addPointToMap);
-    })
+async function addMarker(mapSource, point, markerStyle, markerText){
 
-    function addPointToMap(point) {
+    //let [someResult, anotherResult] = await Promise.all([requestUsers(),requestUsers()]);
+    //console.log(someResult)
         const iconFeature = new ol.Feature({
             geometry: new ol.geom.Point(ol.proj.fromLonLat([point.gps_lon, point.gps_lat])),
-            name: 'Whatever'
+            name: markerText
         });
         iconFeature.setStyle(markerStyle);
         mapSource.addFeature(iconFeature);
-    }
 }
 
-function addCheckpoints(mapSource) {
-    addMarker(mapSource, styles['Checkpoint'], requestCheckpoints)
+async function addCheckpoints(mapSource) {
+    let checkpoints = await requestCheckpoints();
+    checkpoints.forEach( (point)=> {addMarker(mapSource, point, styles['Checkpoint'], `GPS: lon= ${point.gps_lon}, lat= ${point.gps_lon}`)})
+
 }
 
-function addUsers(mapSource) {
-    addMarker(mapSource, styles['User'], requestUsers)
+async function addUsers(mapSource) {
+    let [users, memberships] = await Promise.all([requestUsers(),requestMemberships()]);
+    users.forEach( (point)=> {
+        const team = memberships.find(membership => membership.user == point.user).team
+        let style
+        if (team in teamStyleMapping) {
+            style = teamStyleMapping[team]
+        }
+        else {
+            console.log(styles['Users'][Object.keys(teamStyleMapping).length])
+            style = styles['Users'][Object.keys(teamStyleMapping).length]
+            teamStyleMapping[team] = style
+        }
+        addMarker(mapSource, point, style, `Team: ${team}`)})
 }
 
 
