295c6b241bfa9d4018a7166d1a960012bc3236bf
Pijukatel <Pijukatel@users.noreply.github.com>
2022-03-10 18:26:06 +0100

    Struggling with static files.

--- CheckPointCompetition/settings.py
+++ CheckPointCompetition/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+
+import django_heroku
 from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
